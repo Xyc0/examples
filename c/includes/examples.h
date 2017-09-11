786372d916cab3c3de52d6f4fe82a294f59aaabf
Xyc0 <rndpwn@gmail.com>
2017-09-11 15:49:00 +0000

    format

--- c/includes/examples.h
+++ c/includes/examples.h
@@ -4,25 +4,35 @@
 #author JoYo
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /* Debug Strings */
 
 #ifdef DEBUG
-#   define DEBUG_PRINT( ... )    fprintf(stderr, __VA_ARGS__ );
-#   define ERROR_NOT_ZERO( ... ) if(returnValue != 0){ fprintf( stderr, "error " __VA_ARGS__ ); goto cleanUp; }
+#define DEBUG_PRINT(...) fprintf(stderr, __VA_ARGS__);
+#define ERROR_NOT_ZERO(...)                                                    \
+    if (returnValue != 0)                                                      \
+    {                                                                          \
+        fprintf(stderr, "error " __VA_ARGS__);                                 \
+        goto cleanUp;                                                          \
+    }
 #else
-#   define DEBUG_PRINT( ... )
-#   define ERROR_NOT_ZERO( ... ) if(returnValue != 0){ goto cleanUp; }
+#define DEBUG_PRINT(...)
+#define ERROR_NOT_ZERO(...)                                                    \
+    if (returnValue != 0)                                                      \
+    {                                                                          \
+        goto cleanUp;                                                          \
+    }
 #endif
 
 #define NULL_TERMINATED ('\0')
-#define NODE_END        (0xFFFFFFFFF)
+#define NODE_END (0xFFFFFFFFF)
 
 /* Scrap Structures */
 
-typedef struct scrapNode{
+typedef struct scrapNode
+{
     void *forward;
     void *back;
     void *data;
@@ -30,65 +40,26 @@ typedef struct scrapNode{
 
 /* Strings */
 
-static int
-findFirstUnique(
-    char *string,
-    char *character
-    );
+static int findFirstUnique(char *string, char *character);
 
-static int
-removeDuplicates(
-    char *string
-    );
+static int removeDuplicates(char *string);
 
-static int
-reverseString(
-    char *string,
-    int   length
-    );
+static int reverseString(char *string, int length);
 
-static int
-reverseStringWords(
-    char *string,
-    char *delimiter
-    );
+static int reverseStringWords(char *string, char *delimiter);
 
-static int
-stringLength(
-    char *string,
-    int  *length
-    );
+static int stringLength(char *string, int *length);
 
-static int
-stripString(
-    char *string,
-    char *character
-    );
-
-
-
+static int stripString(char *string, char *character);
 
 /* Linked Lists */
 
 /* #todo */
-static int
-insertNode(
-    scrapNode *listHead,
-    void      *data
-    );
+static int insertNode(scrapNode *listHead, void *data);
 
 /* #todo */
-static int
-deleteNode(
-    scrapNode *listHead,
-    void      *deleteMe
-    );
+static int deleteNode(scrapNode *listHead, void *deleteMe);
 
 /* #todo */
-static int
-splitList(
-    scrapNode *listHead,
-    int        pivot,
-    scrapNode *lt,
-    scrapNode *gt
-    );
+static int splitList(scrapNode *listHead, int pivot, scrapNode *lt,
+                     scrapNode *gt);
